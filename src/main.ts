d3603d98a5688449118b599f677d92c191582166
alvarigno <alvaro.emparan@gmail.com>
2019-07-15 23:09:07 -0400

    add new files accordion

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,12 @@ import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 import { AppModule } from "./app/app.module";
 import { environment } from "./environments/environment";
 
+import { Accordion } from "./accordion.component";
+import { AccordionItem } from "./accordion-item.component";
+
+export { Accordion } from "./accordion.component";
+export { AccordionItem } from "./accordion-item.component";
+
 if (environment.production) {
 	enableProdMode();
 }
